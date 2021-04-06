85849d161530ed52cf06b307f182a7e93340cfbe
= <fboccacini@gmail.com>
2021-04-06 12:44:06 +0900

    backend init

---
+++ backend/src/index.js
@@ -0,0 +1,51 @@
+const { PrismaClient } = require('@prisma/client')
+const { ApolloServer } = require('apollo-server');
+const { PubSub } = require('apollo-server')
+
+const { getUserId } = require('./utils');
+const fs = require('fs');
+const path = require('path');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
+const Subscription = require('./resolvers/Subscription')
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote,
+}
+
+const prisma = new PrismaClient()
+const pubsub = new PubSub()
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
